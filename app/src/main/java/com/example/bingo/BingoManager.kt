8ce01b2d61d8dddf7d2a4142a473dfa96305fbc5
kami_yuta <k.yuta.engineer@gmail.com>
2021-12-12 18:17:16 +0900

    スタートボタンを押すと抽選されるビンゴナンバーをランダムでロールさせ ストップボタンを押すとロールが止まり抽選ナンバーを表示する

--- app/src/main/java/com/example/bingo/BingoManager.kt
+++ app/src/main/java/com/example/bingo/BingoManager.kt
@@ -2,11 +2,12 @@ package com.example.bingo
 
 class BingoManager {
     var bingonums = (1..75).toMutableList()
+    private val rollNumbers = bingonums
     var nextNumber = 0
     var lotteryNum = ""
     var lotteryNumList = mutableListOf<String>()
-    private val end = "終了"
 
+    //履歴を表示させる処理
     fun lotteryHistory():String {
         if (nextNumber > 0) {
             lotteryNum += "$nextNumber,"
@@ -14,6 +15,7 @@ class BingoManager {
         return lotteryNum
     }
 
+    //次のナンバーを表示させる処理
     fun nextBingo(): String {
         return if (lotteryNumList.size != 75) {
             nextNumber = bingonums.random()
@@ -21,8 +23,16 @@ class BingoManager {
             lotteryNumList.add(nextNumber.toString())
             nextNumber.toString()
         } else {
+            val end = "終了"
             nextNumber = 0
             end
         }
+    }
+
+    //ナンバーロールをランダム表示させる処理
+    fun bingoRoll():String {
+        var rollnumber = ""
+        rollnumber = rollNumbers.random().toString()
+        return rollnumber
     }
 }
--- app/src/main/java/com/example/bingo/MainActivity.kt
+++ app/src/main/java/com/example/bingo/MainActivity.kt
@@ -2,21 +2,38 @@ package com.example.bingo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.example.bingo.databinding.ActivityMainBinding
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
 
+    private val handler = Handler(Looper.getMainLooper())
     private val bingoManager = BingoManager()
 
+
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //クリックすると上部にランダムナンバーをロールさせる
         binding.lotteryStart.setOnClickListener {
-            binding.resultListView.text = bingoManager.lotteryHistory()
-            binding.resultText.text = bingoManager.nextBingo()
+            timer(period = 100) {
+                handler.post {
+                    binding.resultText.text = bingoManager.bingoRoll()
+                }
+                //クリックするとビンゴナンバーが出る、2回目から下に履歴として表示される
+                //ランダムナンバーのロールを止める
+                binding.lotteryStop.setOnClickListener {
+                    cancel()
+                    binding.resultListView.text = bingoManager.lotteryHistory()
+                    binding.resultText.text = bingoManager.nextBingo()
+                }
+            }
         }
     }
 }
